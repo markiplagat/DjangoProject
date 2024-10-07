4409c0d08960101dde9ef4eb1f919d3f0f5d5c0c
markiplagat <markkiplagatkipruttoh@gmail.com>
2024-10-08 00:36:44 +0300

    Add templates for render

--- DjangoProject/views.py
+++ DjangoProject/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse("My About Page")
+    return render(request, 'about.html')
